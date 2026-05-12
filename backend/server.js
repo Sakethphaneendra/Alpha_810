b3fbe7aaf1c1542cbb65a5b04898ce8e33fd2a46
Saketh <sakethphaneendra@gmail.com>
2026-05-12 21:49:00 +0530

    Updated Render Port

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,8 @@ app.delete("/tasks/:id", async (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server Running On Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running On Port ${PORT}`);
 });
